5459c6f3e442e65d567df87877ed5e6e4a02a770
Skulsik_coder <skulsik1@gmail.com>
2023-02-21 18:30:22 +0700

    Модуль Datastruct. Задача 2.

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 from utils.utils import *
 
 
-# Проверка логики по ТЗ
-n1 = Node(5, None)
-n2 = Node('a', n1)
-print(n1.data)
-print(n2.data)
-print(n1)
-print(n2.next)
+# Проверка логики по ТЗ-1
+# n1 = Node(5, None)
+# n2 = Node('a', n1)
+# print(n1.data)
+# print(n2.data)
+# print(n1)
+# print(n2.next)
+#
+# stack = Stack()
+# stack.push('data1')
+# stack.push('data2')
+# stack.push('data3')
+# print(stack.top.data)
+# print(stack.top.next.data)
+# print(stack.top.next.next.data)
+# print(stack.top.next.next.next)
+# print(stack.top.next.next.next.data)
 
+# Проверка логики по ТЗ-2
 stack = Stack()
 stack.push('data1')
-stack.push('data2')
-stack.push('data3')
-print(stack.top.data)
-print(stack.top.next.data)
-print(stack.top.next.next.data)
-print(stack.top.next.next.next)
-#print(stack.top.next.next.next.data)
+data = stack.pop()
+
+print(stack.top)
+
+print(data)
+
+stack1 = Stack()
+stack1.push('data1')
+stack1.push('data2')
+data = stack1.pop()
+
+# теперь последний элемента содержит данные data1
+print(stack1.top.data)
+
+# данные удаленного элемента
+print(data)
--- utils/utils.py
+++ utils/utils.py
@@ -16,6 +16,7 @@ class Stack:
         """
         self.top = None
 
+
     def push(self, data):
         """
         Механизм передачи предидущего экземпляра -> текущему (созданному)
@@ -25,3 +26,13 @@ class Stack:
         new_node = Node(data)
         new_node.next = self.top
         self.top = new_node
+
+
+    def pop(self):
+        """
+        Удаление последнего элемента стэка
+        :return: Возврат значения последнего элемента
+        """
+        data = self.top.data
+        self.top = self.top.next
+        return data
